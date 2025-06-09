6e106de9149cd0ec93ef690c1eef3c52d72cebda
wslima_meli <willdslima@gmail.com>
2025-06-09 19:15:21 -0300

    TODO: GlobalExceptionHandler

---
+++ src/main/java/com/module/pix/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.module.pix.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // TODO: Tratar exceptions aqui, para padronizar as respostas http q do json, valid, e outros nao esquecer.
+}
