eaf9c5a5ca993dfbdcc18bcc026d9f856df737dc
wslima_meli <willdslima@gmail.com>
2025-06-09 18:56:32 -0300

    responseDto com build

--- src/main/java/com/module/pix/dto/PixKeyResponseDTO.java
+++ src/main/java/com/module/pix/dto/PixKeyResponseDTO.java
@@ -1,38 +1,35 @@
 package com.module.pix.dto;
 
+import com.module.pix.entity.PixKeyEntity;
+import com.module.pix.enums.KeyTypeEnum;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class PixKeyResponseDTO {
 
-    @NotNull(message = "Tipo da chave deve ser informado")
-    private String keyType;
-
-    @NotBlank(message = "Valor da chave deve ser informado")
+    private UUID id;
+    private KeyTypeEnum keyType;
     private String keyValue;
-
-    @NotNull(message = "Tipo da conta deve ser informado")
-    @Pattern(regexp = "corrente|poupanca", flags = Pattern.Flag.CASE_INSENSITIVE, message = "Tipo de conta deve ser 'corrente' ou 'poupanca'")
-    @Size(max = 10, message = "Tipo de conta não pode ultrapassar 10 caracteres")
     private String accountType;
-
-    @NotNull(message = "Número da agência deve ser informado")
-    @Digits(integer = 4, fraction = 0, message = "Número da agência deve conter até 4 dígitos numéricos")
     private Integer agencyNumber;
-
-    @NotNull(message = "Número da conta deve ser informado")
-    @Digits(integer = 8, fraction = 0, message = "Número da conta deve conter até 8 dígitos numéricos")
     private Integer accountNumber;
-
-    @NotBlank(message = "Nome do correntista deve ser informado")
     private String firstName;
-
     private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public static PixKeyResponseDTO buildPixUUID(PixKeyEntity pixKey) {
+        return PixKeyResponseDTO.builder()
+                .id(pixKey.getId())
+                .build();
+    }
 
 }
