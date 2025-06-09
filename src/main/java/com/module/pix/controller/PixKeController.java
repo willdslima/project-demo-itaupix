799cc86bba9b53a0ca526d688f4d5d243d761611
wslima_meli <willdslima@gmail.com>
2025-06-09 10:24:32 -0300

    Estrutura inicial -  comentarios resumo para desenvolvedor

--- src/main/java/com/module/pix/controller/PixKeController.java
+++ src/main/java/com/module/pix/controller/PixKeController.java
@@ -2,10 +2,7 @@ package com.module.pix.controller;
 
 import com.module.pix.dto.PixKeyRequestDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -14,7 +11,43 @@ import java.util.UUID;
 public class PixKeController {
     @PostMapping
     public ResponseEntity<UUID> cadastrar (@RequestBody PixKeyRequestDTO pixKeyRequestDTO){
+        // Validar tipo de chave
+        // NOK campos, limite e se existe, "Erros distintos"
+        // OK salvar dados complestos
+        // response cadastro realizado
         return ResponseEntity.ok(null);
     }
+
+    @GetMapping
+    public ResponseEntity<UUID> pesquisar (@RequestBody PixKeyRequestDTO pixKeyRequestDTO){
+        // Consultar por id?
+        //      yes, busca por id (if else de encontrado ou dados
+        //      no, filtros validos? (case )
+        //           no-> response filtros invalidos ou combinados
+        //           yes-> busca por filtros
+        //      busca: nok nao encontrado / ok retorna lista de chaves
+
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping
+    public ResponseEntity<UUID> atualizar (@RequestBody PixKeyRequestDTO pixKeyRequestDTO){
+        //busca por id
+        //  desativada (chave inativa) / nao contrada (chave nao encontrada)
+        //  encontrada -> validar campos alteraves, NOK campos invalidos
+        //      atualizar dados permitidos
+        //      response alteracao realizada
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping
+    public ResponseEntity<UUID> deletar (@RequestBody PixKeyRequestDTO pixKeyRequestDTO){
+        // busca por id
+        //      Nao contrada(id nao encontrada) / Ja ativa(jave ja invativada)
+        // Ativa (marca como inativa + data/hora da inativacao
+
+        return ResponseEntity.ok(null);
+    }
+
 
 }
