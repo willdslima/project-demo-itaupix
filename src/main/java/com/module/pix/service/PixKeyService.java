992cab7af9c9f69801b2bff23c9730d8ff5246ad
wslima_meli <willdslima@gmail.com>
2025-06-11 13:59:19 -0300

    search -> controller, service, validation, build search, refactor specification likeDataPattern

--- src/main/java/com/module/pix/service/PixKeyService.java
+++ src/main/java/com/module/pix/service/PixKeyService.java
@@ -2,13 +2,18 @@ package com.module.pix.service;
 
 import com.module.pix.dto.PixKeyRequestDTO;
 import com.module.pix.dto.PixKeyResponseDTO;
+import com.module.pix.dto.PixKeySearchDTO;
 import com.module.pix.dto.PixKeyUpdateDTO;
 import com.module.pix.entity.PixKeyEntity;
+import com.module.pix.exception.ResourceNotFoundException;
 import com.module.pix.repository.PixKeyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +35,21 @@ public class PixKeyService {
 
         PixKeyEntity updated = PixKeyEntity.buildUpdateEntity(pixKeyUpdateDTO, pixKeyEntity);
 
-        return PixKeyResponseDTO.buildResponseUpdatedDTO(pixKeyRepository.save(updated));
+        return PixKeyResponseDTO.buildResponseDTO(pixKeyRepository.save(updated));
+    }
+
+    public List<PixKeyResponseDTO> getPixKeys(PixKeySearchDTO pixKeySearchDTO) {
+        Specification<PixKeyEntity> specification = validationService.validateFilterForSearch(pixKeySearchDTO);
+
+        List<PixKeyEntity> pixKeys = pixKeyRepository.findAll(specification);
+
+        if (pixKeys.isEmpty()) {
+            throw new ResourceNotFoundException("Nada encontrado para os filtros informados.");
+        }
+
+        return pixKeys.stream()
+                .map(PixKeyResponseDTO::buildResponseDTO)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/module/pix/utils/PixKeySpecificationBuilder.java
+++ src/main/java/com/module/pix/utils/PixKeySpecificationBuilder.java
@@ -5,21 +5,25 @@ import com.module.pix.entity.PixKeyEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 @Component
 public class PixKeySpecificationBuilder {
 
-    public static Specification<PixKeyEntity> buildSpecificFilter(PixKeySearchDTO dto) {
-        if (dto.getId() != null) {
-            return(root, query, cb) -> cb.equal(root.get("id"), dto.getId());
+    public static Specification<PixKeyEntity> buildSpecificFilter(PixKeySearchDTO pixKeySearchDTO) {
+        if (pixKeySearchDTO.getId() != null) {
+            return (root, query, cb) -> cb.equal(root.get("id"), pixKeySearchDTO.getId());
         }
 
-        return Specification.allOf(equal("keyType", dto.getKeyType()))
-                .and(equal("agencyNumber", dto.getAgencyNumber()))
-                .and(equal("accountNumber", dto.getAccountNumber()))
-                .and(likeIgnoreCase("firstName", dto.getFirstName()))
-                .and(equal("createdAt", dto.getCreatedAt()))
-                .and(equal("updatedAt", dto.getUpdatedAt()))
-                .and(equal("deactivationDate", dto.getDeactivationDate()));
+        return Specification
+                .allOf(equal("keyType", pixKeySearchDTO.getKeyType()))
+                .and(equal("agencyNumber", pixKeySearchDTO.getAgencyNumber()))
+                .and(equal("accountNumber", pixKeySearchDTO.getAccountNumber()))
+                .and(likeIgnoreCase("firstName", pixKeySearchDTO.getFirstName()))
+                .and(likeDatePattern("createdAt", pixKeySearchDTO.getCreatedAt() != null ? pixKeySearchDTO.getCreatedAt() : null))
+                .and(likeDatePattern("updatedAt", pixKeySearchDTO.getUpdatedAt() != null ? pixKeySearchDTO.getUpdatedAt() : null));
     }
 
     private static <T> Specification<PixKeyEntity> equal(String field, T value) {
@@ -30,6 +34,19 @@ public class PixKeySpecificationBuilder {
     private static Specification<PixKeyEntity> likeIgnoreCase(String field, String value) {
         return (value == null || value.isBlank()) ? null :
                 (root, query, cb) -> cb.like(cb.lower(root.get(field)), "%" + value.toLowerCase() + "%");
+    }
+
+    private static Specification<PixKeyEntity> likeDatePattern(String field, String pattern) {
+        if (pattern == null || pattern.isBlank()) return null;
+
+        String isoPattern = pattern;
+        try {
+            isoPattern = LocalDate.parse(pattern, DateTimeFormatter.ofPattern("dd-MM-yyyy")).toString();
+        } catch (DateTimeParseException ignored) {}
+
+        String finalPattern = "%" + isoPattern + "%";
+        return (root, query, cb) ->
+                cb.like(cb.function("to_char", String.class, root.get(field), cb.literal("YYYY-MM-DD")), finalPattern);
     }
 
 }
